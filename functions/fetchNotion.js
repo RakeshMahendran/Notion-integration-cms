b57bab2097e7f44e367ca3801b5bdb360401b791
RakeshMahendran <rakeshmahendran99@gmail.com>
2022-10-09 04:13:07 +0530

    updated and finalized

--- functions/fetchNotion.js
+++ functions/fetchNotion.js
@@ -1,37 +1,32 @@
- const { Client } = require("@notionhq/client");
+const { Client } = require('@notionhq/client');
 
- const { NOTION_KEY , NOTION_DB } = process.env;
+const { NOTION_KEY, NOTION_DB } = process.env;
 
 // Initializing a client
-  const notion = new Client({
-  auth: NOTION_KEY,
-  })
-
-
+const notion = new Client({
+    auth: NOTION_KEY,
+});
 
 exports.handler = async function (event, context) {
-    try{
+    try {
         const response = await notion.databases.query({
-           database_id: NOTION_DB,
-           filter: {
-            "property": "Status",
-            "select":{
-                "equals":"Live",
+            database_id: NOTION_DB,
+            filter: {
+                property: 'Status',
+                select: {
+                    equals:'Live',
+                },
             },
-           },
-    });
+        });
         return {
             statusCode: 200,
             body: JSON.stringify(response),
         };
-    }
-    catch (e) {
+    } catch (e) {
         console.error(e);
         return {
             statusCode: 500,
             body: e.toString(),
-        }
+        };
     }
-    
-    
- };
+};
